515e8e8375926c814f8d3c95f493208f4c5995e9
Warlord5588 <56461130+Warlord5588@users.noreply.github.com>
2019-10-29 18:41:13 +0700

    Finished Bmi and Tdee

---
+++ Project_Calculated.py
@@ -0,0 +1,49 @@
+"""Calculated"""
+def bmi():
+	"""BMI Calculated"""
+	print("This is BMI Calculater.")
+	print("")
+	weight = float(input("Your weight is "))
+	hight = float(input("Your hight is "))
+	convert = hight / 100
+	bmi = weight / (convert**2)
+	print("Your BMI is %.2f" %bmi)
+
+def tdee():
+	print("")
+	print("This is TDEE Calculater.")
+	print("")
+	gender = input("What is your gender Male or Female ")
+	age = int(input("How old are you "))
+	weight = float(input("Your weight is "))
+	hight = float(input("Your hight is "))
+	bmr = 0
+	if gender == "Male":
+		bmr = 66 + (13.7 * weight) + (5 * hight) - (6.8 * age)
+	elif gender == "Female":
+		bmr = 665 + (9.6 * weight) + (1.8 * hight) - (4.7 * age)
+
+	print("Press 1 if you don't have any workout")
+	print("Press 2 if you have a workout 1-3 times per week")
+	print("Press 3 if you have a workout 3-5 times per week")
+	print("Press 4 if you have a workout 6-7 times per week")
+	print("Press 5 if you workout so hard everday")
+	tdee = 0
+	answer = input()
+	if answer == "1":
+		tdee = 1.2 * bmr
+	elif answer == "2":
+		tdee = 1.375 * bmr
+	elif answer == "3":
+		tdee = 1.55 * bmr
+	elif answer == "4":
+		tdee = 1.725 * bmr
+	elif answer == "5":
+		tdee = 1.9 * bmr
+	else:
+		print("Please answer the right choice 1,2,3,4 or 5")
+		tdee()
+	print("Your TDEE is %.2f" %tdee)
+
+bmi()
+tdee()
